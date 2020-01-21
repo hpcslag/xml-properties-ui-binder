66e9cda0abf92873a7130e7c7047f95fe119242d
hpcslag <cslag@hotmail.com.tw>
2020-01-21 15:41:31 +0800

    init project

---
+++ XMLPropertiesUIBinder/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Xml;
+
+namespace XMLPropertiesUIBinder
+{
+	class Program { 
+		private const string TestXml =
+			"<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+			"<MovieData>" +
+				"<Movies>" +
+					"<Movie>" +
+						"<Id>1</Id>" +
+						"<Name>Batman</Name>" +
+					"</Movie>" +
+					"<Movie>" +
+						"<Id>2</Id>" +
+						"<Name>Batman Returns</Name>" +
+					"</Movie>" +
+					"<Movie>" +
+						"<Id>3</Id>" +
+						"<Name>Batman Dark Knight</Name>" +
+					"</Movie>" +
+				"</Movies>" +
+			"</MovieData>";
+
+
+		public static void Main()
+		{
+
+			TableBinder bindingObject = new TableBinder();
+			bindingObject.bindRepeatNode("/MovieData/Movies/Movie");
+			bindingObject.setPropertyColumn("Id", "Movie ID", "TextInput");
+			bindingObject.setPropertyColumn("Name", "Movie Name", "TextInput");
+
+
+
+			XmlDocument xmlDoc = new XmlDocument();
+			xmlDoc.LoadXml(TestXml);
+
+
+			//XmlNode xnl = xmlDoc.SelectNodes;
+			//Console.WriteLine(xnl);
+
+
+			/*string xpath = "MovieData/Movies/Movie";
+			var nodes = xmlDoc.SelectNodes(xpath);
+
+			foreach (XmlNode childrenNode in nodes)
+			{
+				Console.WriteLine(
+					"Movie - Id: " + childrenNode.SelectSingleNode(".//Id").InnerText +
+					", Name: " + childrenNode.SelectSingleNode(".//Name").InnerText);
+			}*/
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ XMLPropertiesUIBinder/TableBinder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLPropertiesUIBinder
+{
+    class TableBinder
+    {
+
+        private struct property {
+            public string pname;
+            public string description;
+            public string type;
+
+            public property(string pname, string description, string type)
+            {
+                this.pname = pname;
+                this.description = description;
+                this.type = type;
+            }
+        }
+
+        string parent = "";
+
+        public void bindRepeatNode(string path)
+        {
+            this.parent = path;
+        }
+
+        private List<property> propertiesColumn = new List<property>();
+        public void setPropertyColumn(string pname, string description, string type)
+        {
+            propertiesColumn.Add(new property (pname, description, type));
+        }
+
+        public void showData() {
+            foreach (XmlNode childrenNode in nodes)
+            {
+                foreach(property p in propertiesColumn)
+                {
+                    Console.WriteLine(
+                    "Movie - Id: " + childrenNode.SelectSingleNode(p.pname).InnerText +
+                    ", Name: " + childrenNode.SelectSingleNode(".//Name").InnerText);
+                }
+                
+            }
+        }
+    }
+}
